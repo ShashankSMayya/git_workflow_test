c2e1f2da5a55594fb951709a1eff3d19dc305d1a
Shashank S Mayya <shashanksmayya@gmail.com>
2022-12-27 11:53:16 +0530

    feature:MEVO-1904-added habit screen (#6)
    feature:MEVO-1904-added habit screen

---
+++ lib/habit_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HabitScreen extends StatelessWidget {
+  const HabitScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello')),
+    );
+  }
+}
